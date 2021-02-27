f126d03a74b87d37d9cef9d0f29a5bc8d66cc3ea
Mihai H <59961959+automatica-cluj@users.noreply.github.com>
2021-02-27 10:39:53 +0200

    Primul program Java Satu-Mare

---
+++ curs-isp-2021-sm/src/main/java/isp/curs/sm/curs1/HelloSM.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isp.curs.sm.curs1;
+
+/**
+ *
+ * @author mihai.huleal
+ */
+public class HelloSM {
+    public static void main(String[] args) {
+        System.out.println("It works!");
+    }
+}
